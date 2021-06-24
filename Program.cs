01ac1b7181ec27f2fd885a6d16d67038187bde15
Vitaliy Bogatov <x-vitaliy.bogatov@coyote.com>
2021-06-24 16:49:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TI_Practice9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
